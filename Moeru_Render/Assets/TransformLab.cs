b02c5760ad900fa72310f95e2891728eb6f55370
ShinomoriRinonn <569886545@qq.com>
2021-01-02 18:35:40 +0800

    monster2001003 文件路径调整

---
+++ Moeru_Render/Assets/TransformLab.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class TransformLab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera mainCamera; 
+    void Start()
+    {
+        mainCamera = Camera.main;
+        // gameObject.transform.LookAt(mainCamera.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(mainCamera.transform);
+    }
+}
